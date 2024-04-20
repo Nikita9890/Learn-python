58d9c7f3a27a519cdcd3da047962e930f16326b8
Nikita <nikitr00n00@gmail.com>
2024-04-20 12:36:06 +0300

    UPD

---
+++ learn functii.py
@@ -0,0 +1,12 @@
+def f():
+    #return x + y * z
+    a = 1 +2 + 3
+result = f()
+print(result)
+
+
+#if a == 3:
+    #print("a равно 3")
+
+#else:
+    #print("z не равно 3")
---
+++ learn in functii.py
@@ -0,0 +1,6 @@
+age = input("Please input your age:")
+int_age = int(age)
+if int_age < 21:
+    print("nice")
+else:
+    print("BAD")
