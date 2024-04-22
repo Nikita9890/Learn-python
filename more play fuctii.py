460ada981921b5c00d4d7dc63447d78926ce9a2f
Nikita <nikitr00n00@gmail.com>
2024-04-23 00:14:57 +0300

    UPD

---
+++ more play fuctii.py
@@ -0,0 +1,28 @@
+def f(x):
+    return x**2
+print(f(4))
+
+def stroka(x):
+    return x
+print(stroka("Привет"))
+
+def y(x, n, p=2, z=4, b=9):
+    return x + n + p + z + b
+result = y(2,4,8,)
+print(result)
+
+def y(x):
+    return x/2
+result1 = y(4)
+int_result1 = int(result1)
+print(int_result1)
+
+mag = int_result1
+def morg(v):
+    return v*4
+    x = global mag
+morg()
+
+
+
+
